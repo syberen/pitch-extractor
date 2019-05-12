8c240e29f6c01c0a1b2d2362eafb8e9dc73786f6
Syberen <syberen.van.munster@gmail.com>
2019-05-12 20:27:29 +0200

    Accept file path in constructor method

--- extractor.py
+++ extractor.py
@@ -1,39 +1,35 @@
 import math
 
 class PitchExtractor:
+    def __init__(self, file_path):
+        if file_path:
+            self.load_midi(file_path)
+
     def load_midi(self, file_path):
         with open(file_path, 'rb') as midi_file:
             leader = midi_file.read(4)
             if leader != b'MThd':
                 raise ValueError('Invalid file type')
 
-            self.data_hex_bytes = midi_file.read().hex()
+            data_hex_bytes = midi_file.read().hex()
+            pitches = self._parse_hex_bytes(data_hex_bytes)
 
-            self.parse_hex_file(self.data_hex_bytes)
+            self._pitches = pitches
 
-    def parse_hex_file(self, data):
+    def _parse_hex_bytes(self, data):
         pitches = []
 
         for index, byte in enumerate(data):
-            is_pitch = index % 2 ==0 and byte == '9'
+            is_pitch = index % 2 == 0 and byte == '9'
             is_note_on = data[index + 4 : index + 6] != '00'
 
             if is_pitch and is_note_on: 
                 pitch_hex = data[index + 2 : index + 4]
                 pitches.append(int(pitch_hex, 16))
                 
-        self.pitches = pitches
+        return pitches
         
-    def get_pitches(self, note_names=False):
-        if not self.pitches:
-            raise ValueError('No pitches found')
-
-        if note_names:
-            return self.get_note_names(self.pitches)
-
-        return self.pitches
-        
-    def get_note_names(self, pitches):
+    def _get_note_names(self, pitches):
         notes = []
         pitch_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
@@ -44,5 +40,14 @@ class PitchExtractor:
             notes.append(pitch_name + str(octave))
           
         return notes
+        
+    def pitches(self, note_names=False):
+        if not self._pitches:
+            raise ValueError('No pitches found')
+
+        if note_names:
+            return self._get_note_names(self._pitches)
+
+        return self._pitches
 
             
