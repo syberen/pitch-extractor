b089ecc71ded3fcf435b7b62fa4390fd3d2e9066
Syberen van Munster <syberen@wend.nl>
2019-05-20 17:26:21 +0200

    Add unit tests

--- extractor.py
+++ extractor.py
@@ -1,5 +1,6 @@
 import math
 
+
 class PitchExtractor:
     def __init__(self, file_path):
         if file_path:
@@ -20,26 +21,27 @@ class PitchExtractor:
 
         for index, byte in enumerate(self._data_hex):
             is_pitch = index % 2 == 0 and byte == '9'
-            is_note_on = self._data_hex[index + 4 : index + 6] != '00'
+            is_note_on = self._data_hex[index + 4: index + 6] != '00'
 
-            if is_pitch and is_note_on: 
-                pitch_hex = self._data_hex[index + 2 : index + 4]
+            if is_pitch and is_note_on:
+                pitch_hex = self._data_hex[index + 2: index + 4]
                 pitches.append(int(pitch_hex, 16))
-                
+
         return pitches
-        
+
     def _get_note_names(self, pitch_list):
         notes = []
-        pitch_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        pitch_names = ['C', 'C#', 'D', 'D#', 'E',
+                       'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
         for pitch in pitch_list:
             octave = math.floor(pitch / 12)
             pitch_name = pitch_names[pitch - (12 * octave)]
 
             notes.append(pitch_name + str(octave))
-          
+
         return notes
-        
+
     def pitches(self, note_names=False):
         if not self._data_hex:
             raise ValueError('No file loaded')
@@ -50,5 +52,3 @@ class PitchExtractor:
             return self._get_note_names(pitch_list)
 
         return pitch_list
-
-            
